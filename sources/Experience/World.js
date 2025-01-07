7cae53ec7cc290bf9ceff4cc08f759ec4c7c7fe1
Jasmeher <bharadwajjasmeher@gmail.com>
2025-01-07 21:03:39 +0530

    updated version of the project changed lil-gui for tweakpane

--- sources/Experience/World.js
+++ sources/Experience/World.js
@@ -1,44 +1,33 @@
-import * as THREE from 'three'
-import Experience from './Experience.js'
+import * as THREE from "three";
+import Experience from "./Experience.js";
 
-export default class World
-{
-    constructor(_options)
-    {
-        this.experience = new Experience()
-        this.config = this.experience.config
-        this.scene = this.experience.scene
-        this.resources = this.experience.resources
-        
-        this.resources.on('groupEnd', (_group) =>
-        {
-            if(_group.name === 'base')
-            {
-                this.setDummy()
-            }
-        })
-    }
+export default class World {
+  constructor(_options) {
+    this.experience = new Experience();
+    this.config = this.experience.config;
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
 
-    setDummy()
-    {
-        this.resources.items.lennaTexture.encoding = THREE.sRGBEncoding
-        
-        const cube = new THREE.Mesh(
-            new THREE.BoxGeometry(1, 1, 1),
-            new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
-        )
-        this.scene.add(cube)        
-    }
+    this.resources.on("groupEnd", (_group) => {
+      if (_group.name === "base") {
+        this.setDummy();
+      }
+    });
+  }
 
-    resize()
-    {
-    }
+  setDummy() {
+    this.resources.items.lennaTexture.colorSpace = THREE.SRGBColorSpace;
 
-    update()
-    {
-    }
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
+    );
+    this.scene.add(cube);
+  }
 
-    destroy()
-    {
-    }
+  resize() {}
+
+  update() {}
+
+  destroy() {}
 }
